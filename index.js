5d56fdacf24dd3e9fdca45cac7faddc3b31521df
catalinafs <catalinaforerosuarez@gmail.com>
2024-02-23 17:59:03 -0500

    feedback

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 //constantes de elementos HTML
-const num = document.getElementById("num")
-const decrease = document.getElementById("decrease")
-const increase = document.getElementById("increase")
-const reset = document.getElementById("reset")
+const num = document.getElementById("num");
+const decrease = document.getElementById("decrease");
+const increase = document.getElementById("increase");
+const reset = document.getElementById("reset");
 
-let numero = 0
-num.innerHTML = numero
+let numero = 0;
+num.innerHTML = numero;
 
 //Configuración de botones
 decrease.onclick = disminuir;
 increase.onclick = aumentar;
 reset.onclick = resetear;
 
+// if (0 === false) {
+//     console.log('holis')
+// } else {
+//     console.log('chao')
+// }
+
+// 0 === false ? console.log('holis') : console.log('chao');
+
 //Funciones
-function disminuir(){
-    numero -= 1
-    num.innerHTML = numero
-    num.style.color = 'rgb(128, 0, 0)'
+function disminuir() {
+    numero -= 1;
+    num.innerHTML = numero;
+    num.style.color = 'rgb(128, 0, 0)';
 }
 
-function aumentar(){
-    numero += 1
-    num.innerHTML = numero
-    num.style.color = 'rgb(0, 128, 0)'
+function aumentar() {
+    numero += 1;
+    num.innerHTML = numero;
+    num.style.color = 'rgb(0, 128, 0)';
 }
 
-function resetear(){
-    numero = 0
-    num.innerHTML = numero
-    num.style.color = 'rgb(255, 255, 255)'
+function resetear() {
+    numero = 0;
+    num.innerHTML = numero;
+    num.style.color = 'rgb(255, 255, 255)';
 }
